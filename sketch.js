fff5c6770ef3e216a47d547e0821b9b930d4935f
victor <victordj587@gmail.com>
2022-02-14 21:32:21 +0100

    fix

--- sketch.js
+++ sketch.js
@@ -1,12 +1,42 @@
+function makeArr2D(rows, cols) {
+  let arr = new Array(cols);
+  for(let i = 0 ; i < arr.length ; i++) {
+    arr[i] = new Array(rows);
+  }
+  return arr;
+}
+
+let canvasHeight = 800;
+let canvasWidth = 800;
 
 let grid;
-let rows = 20;
-let cols = 20;
+let rows;
+let cols;
+let reso = 20;
 
 function setup() {
-  createCanvas(800,800)
+  rows = canvasHeight / reso;
+  cols = canvasWidth / reso;
+  grid = makeArr2D(rows, cols);
+  createCanvas(canvasWidth,canvasHeight);
+  for(let i = 0; i < cols ; i++) {
+    for(let j = 0; j < rows ; j++) {
+      grid[i][j] = floor(random(2))
+    }
+  }
+  print(grid)
 }
 
 function draw() {
   background(12);
+  for(let i = 0; i < cols ; i++) {
+    for(let j = 0; j < rows ; j++) {
+      if(grid[i][j] == 1) {
+        fill(255);
+        let x = i * reso;
+        let y = j * reso;
+        rect(x,y,reso,reso);
+      }
+    }
+  }
 }
